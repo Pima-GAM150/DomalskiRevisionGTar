a23e904b407d941c84798e193c37f28cac6c5223
Evolved Bag <undercloudstudio@gmail.com>
2019-04-15 08:28:18 -0700

    transferring Guitarcade to using update
    used to use a coroutine that just made life harder

--- Assets/Scripts/Guitarcade.cs
+++ Assets/Scripts/Guitarcade.cs
@@ -11,14 +11,108 @@ public class Guitarcade : MonoBehaviour {
 	//public BulletPatterns patternList;
 	public float analysisWait;
 	private float effectiveWait;
+	public bool canFire;
 
 
 
 	void Start () {
 		effectiveWait = analysisWait;
 		currentHealth = maxHealth;
-		StartCoroutine(PatternController());
+		//StartCoroutine(PatternController());
 	
+	}
+
+	void Update(){
+
+		if(canFire){
+			int MIDI = input.FrequencyToMIDI (input.Analyze ());
+
+				if (Input.GetKey (KeyCode.E))
+					BulletPatterns.SplitterPattern1 ();
+
+			int temp = MIDI % 12;
+		
+
+			switch (MIDI) {
+			case 0:
+				label.text = "" + MIDI + ": C";
+					BulletPatterns.TargetedPattern3 ();
+					effectiveWait = 0.6f;
+					canFire = false;
+				break;
+			case 1:
+				label.text = "" + MIDI + ": Db/C#";
+				break;
+			case 2:
+				label.text = "" + MIDI + ": D";
+					BulletPatterns.SplitterPattern1();
+					effectiveWait = 1.2f;
+					canFire = false;
+				break;
+			case 3:
+				label.text = "" + MIDI + ": Eb/D#";
+					BulletPatterns.CirclePattern1 ();
+					effectiveWait = 0.6f;
+					canFire = false;
+				break;
+			case 4:
+				label.text = "" + MIDI + ": E";
+
+				break;
+			case 5:
+				label.text = "" + MIDI + ": F";
+					BulletPatterns.CirclePattern1 ();
+					effectiveWait = 0.6f;
+					canFire = false;
+				break;
+			case 6:
+				label.text = "" + MIDI + ": Gb/F#";
+				break;
+			case 7:
+				label.text = "" + MIDI + ": G";
+					BulletPatterns.CirclePattern2 ();
+					effectiveWait = 0.6f;
+					canFire = false;
+				break;
+			case 8:
+				label.text = "" + MIDI + ": Ab/G#";
+					BulletPatterns.TargetedPattern1 ();
+					effectiveWait = 0.3f;
+					canFire = false;
+				break;
+			case 9:
+				label.text = "" + MIDI + ": A";
+				break;
+			case 10:
+				label.text = "" + MIDI + ": Bb/A#";
+					BulletPatterns.TargetedPattern2 ();
+					effectiveWait = 0.3f;
+					canFire = false;
+				break;
+			case 11:
+				label.text = "" + MIDI + ": B";
+				break;
+
+			default:
+				label.text = "Unrecognized";
+				break;
+
+			}
+		}
+		else{
+
+			analysisWait += Time.deltaTime;
+
+			if(analysisWait >= effectiveWait){
+
+				analysisWait = 0;
+				canFire = true;
+
+			}
+
+		}
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
@@ -40,78 +134,5 @@ public class Guitarcade : MonoBehaviour {
 
 	void GameOver(){}
 
-	IEnumerator PatternController () {
-
-		while (true) {
-		int MIDI = input.FrequencyToMIDI (input.Analyze ());
-
-			if (Input.GetKey (KeyCode.E))
-				BulletPatterns.SplitterPattern1 ();
-
-		int temp = MIDI % 12;
 	
-
-		switch (MIDI) {
-		case 0:
-			label.text = "" + MIDI + ": C";
-				BulletPatterns.TargetedPattern3 ();
-				effectiveWait = 0.8f;
-			break;
-		case 1:
-			label.text = "" + MIDI + ": Db/C#";
-			break;
-		case 2:
-			label.text = "" + MIDI + ": D";
-				BulletPatterns.SplitterPattern1();
-				effectiveWait = 3f;
-			break;
-		case 3:
-			label.text = "" + MIDI + ": Eb/D#";
-				BulletPatterns.CirclePattern1 ();
-				effectiveWait = 0.8f;
-			break;
-		case 4:
-			label.text = "" + MIDI + ": E";
-
-			break;
-		case 5:
-			label.text = "" + MIDI + ": F";
-				BulletPatterns.CirclePattern1 ();
-				effectiveWait = 0.8f;
-			break;
-		case 6:
-			label.text = "" + MIDI + ": Gb/F#";
-			break;
-		case 7:
-			label.text = "" + MIDI + ": G";
-				BulletPatterns.CirclePattern2 ();
-				effectiveWait = 0.8f;
-			break;
-		case 8:
-			label.text = "" + MIDI + ": Ab/G#";
-				BulletPatterns.TargetedPattern1 ();
-				effectiveWait = 0.5f;
-			break;
-		case 9:
-			label.text = "" + MIDI + ": A";
-			break;
-		case 10:
-			label.text = "" + MIDI + ": Bb/A#";
-				BulletPatterns.TargetedPattern2 ();
-				effectiveWait = 0.5f;
-			break;
-		case 11:
-			label.text = "" + MIDI + ": B";
-			break;
-
-		default:
-			label.text = "Unrecognized";
-			break;
-
-		}
-
-			yield return new WaitForSeconds (effectiveWait);
-			effectiveWait = analysisWait;
-	
-		}}
 }
