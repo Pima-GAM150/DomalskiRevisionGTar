5769daa808d12b0f4a3e7b221143a15b75ee1c47
Sarah <undercloudstudio@gmail.com>
2019-04-17 13:42:51 -0700

    Added player bullet script and prefab

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+        if (transform.position.y > 7)
+            DestroyObject(gameObject);
+
+
+    }
+}
