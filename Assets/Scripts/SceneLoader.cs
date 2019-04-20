9bc431a2e66edc05fe649fce3c7d219191676923
Evolved Bag <undercloudstudio@gmail.com>
2019-04-19 18:46:02 -0700

    Added static SceneLoader for a menu system

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneLoader;
+
+public class SceneLoader
+{
+    
+    public static void LoadScene(int x){
+
+    	SceneManager.LoadScene(x);
+
+    }
+
+}
