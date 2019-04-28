63fb40549a6057ebd3595619a557e91bbb09db7c
Evolved Bag <undercloudstudio@gmail.com>
2019-04-27 19:45:47 -0700

    Pulsing bullet script

---
+++ Assets/Scripts/PulseBulletMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseBulletMove : MonoBehaviour
+{
+    public float xdir, ydir, speed, timer;
+    // Update is called once per frame
+    void Update(){
+
+    	float pulsar = Math.Sin(timer);
+    	transform.Translate(new Vector3(xdir, ydir).normalized * ((pulsar + speed) * speed));
+        timer += Time.deltaTime;
+        if(timer > 2f * Math.PI)
+        	timer = 0f;
+
+    }
+}
