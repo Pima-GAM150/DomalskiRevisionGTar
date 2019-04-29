ff7126603bd83a276f0af601181ab9e96e4ac973
Evolved Bag <undercloudstudio@gmail.com>
2019-04-28 20:32:22 -0700

    Serpentine now functioning as intended

--- Assets/Scripts/SerpentineBulletMovement.cs
+++ Assets/Scripts/SerpentineBulletMovement.cs
@@ -11,33 +11,42 @@ public class SerpentineBulletMovement : MonoBehaviour{
 	void Start(){
 
 		//get perpendicular vector
-		perp = new Vector2(1f, ((-1f * xdir)/ ydir)).Normalize();
-
+		perp = new Vector2(1f, ((-1f * xdir)/ ydir));
+		perp.Normalize();
+		//Debug.Log(perp);
 		//get unit vector of direction
-		dir = new Vector2(xdir, ydir).Normalize();
-
+		dir = new Vector2(xdir, ydir);
+		dir.Normalize();
+		//Debug.Log(dir);
 	}
 
 	void Update(){
 
+		//Debug.Log(perp);
 		//this kind of came to me as I was at work and I'm trying to understand my scrawls
 		//so I'm commenting as I type to understand what the hell I'm making here
 
 		// create a magnitude for the bullet to deviate from the path
-		float temp = (float)Math.Sin(timer) * variation; 
+		float temp = (float)Math.Sin(timer) * variation / 5f; 
 
 		//as I understand it at this point I'm using perp and dir as my own axes
 		//I'm using dir as a constant line of movement, and perp as the axis of
 		//variation, if I multiply perp by temp and then add it to dir multiplied by
 		//speed, I should hopefully get the effect I'm looking for?
 
-		transform.Translate((perp * temp) + (dir * speed));
+		Vector2 tempV = (perp * temp) + (dir * speed / 10f);
+
+		Vector3 mov = new Vector3(tempV.x, tempV.y, 0f);
+
+		transform.Translate(mov);
 
 		timer += Time.deltaTime * varSpeed;
 
 		if(timer > 2 * Math.PI)
 			timer = 0f;
 
+		if(transform.position.x > 11 || transform.position.x < -11 || transform.position.y > 8 || transform.position.y < -8)
+			Destroy(this);
 	}
     
 }
