52b3cd543c45f024f40c56d42dcbd163077b3d99
Evolved Bag <undercloudstudio@gmail.com>
2019-04-30 12:33:23 -0700

    Pulsing bullet patterns prefabs

--- Assets/Scripts/PulseBulletMove.cs
+++ Assets/Scripts/PulseBulletMove.cs
@@ -5,13 +5,13 @@ using System;
 
 public class PulseBulletMove : MonoBehaviour
 {
-    public float xdir, ydir, speed, timer;
+    public float xdir, ydir, speed, timer, timerMult, pulseM;
     // Update is called once per frame
     void Update(){
 
-    	float pulsar = (float)Math.Sin(timer);
+    	float pulsar = (float)Math.Sin(timer) * pulseM;
     	transform.Translate(new Vector3(xdir, ydir).normalized * ((pulsar + speed) * speed));
-        timer += Time.deltaTime;
+        timer += Time.deltaTime * timerMult;
         if(timer > 2f * Math.PI)
         	timer = 0f;
 
